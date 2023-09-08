cac195aa5d140af0d8a6872a2619ac2b6827d8dd
Shubham Negi <buhs0111@gmail.com>
2023-09-08 10:42:01 +0530

    day8

---
+++ BFS/LevelOrderTraversal.java
@@ -0,0 +1,36 @@
+package BFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null){
+            return new ArrayList<>(new ArrayList<>(List.of()));
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>(size);
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.remove();
+                temp.add(node.val);
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            list.add(temp);
+        }
+
+        return list;
+    }
+}
